e203f509f2689b7e0773fb18b579ab79a93006fe
113dedmgradution <164179593+113dedmgradution@users.noreply.github.com>
2024-03-20 20:19:46 +0800

    Add files via upload

---
+++ js/loading.js
@@ -0,0 +1,22 @@
+const loadingVideo = document.getElementById('loading-video');
+
+setTimeout(function() {
+  const loadingPage = document.getElementById('loading-page');
+  loadingPage.style.transition = 'opacity 1s';
+  loadingPage.style.opacity = '0';
+
+  setTimeout(function() {
+    loadingPage.parentNode.removeChild(loadingPage);
+    loadingVideo.pause(); 
+  }, 900);
+}, 3100);
+
+
+loadingVideo.addEventListener('loadedmetadata', function() {
+  loadingVideo.play();
+});
+
+
+loadingVideo.addEventListener('ended', function() {
+  loadingVideo.pause();
+});
